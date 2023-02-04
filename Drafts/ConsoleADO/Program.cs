3da6b66137a699bf16e4d03532697aac7576a0a2
Igor <ivanchenko.i91@mail.ru>
2023-02-04 23:09:14 +0600

    Create DB

--- Drafts/ConsoleADO/Program.cs
+++ Drafts/ConsoleADO/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Data.SqlClient;
 
 namespace ConsoleADO
 {
@@ -9,8 +7,39 @@ namespace ConsoleADO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Projects\MyTest\C#\TestProjects\Learn\ADO.NET\Drafts\ConsoleADO\bin\Debug\Resources\ForTraining.mdf;Integrated Security=True;Connect Timeout=30";
 
+
+
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            string sqlExpression = "SELECT model, speed FROM Laptop";
+            
+
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression,connection);
+                SqlDataReader reader = command.ExecuteReader();
+                Console.WriteLine($"{reader.GetName(0)}:\t {reader.GetName(1)}:");
+                while (reader.Read())
+                {
+                    string model = reader.GetString(0);
+                    double speed = double.Parse(reader.GetValue(1).ToString());
+                    Console.WriteLine($"{model}\t {speed/10}x");
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+                Console.WriteLine("Подключение закрыто");
+            }
+
+            Console.WriteLine("End work!");
             Console.ReadKey();
         }
     }
