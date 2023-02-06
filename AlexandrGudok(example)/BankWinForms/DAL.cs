7f26e0be7e160484e3413a36b4ac62ad7a6d773a
Igor <ivanchenko.i91@mail.ru>
2023-02-07 00:09:15 +0600

    End create code with connect DB

---
+++ AlexandrGudok(example)/BankWinForms/DAL.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+
+namespace BankWinForms
+{
+    class DAL
+    {
+
+        /// <summary>
+        /// Строка соединения
+        /// </summary>
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Projects\MyTest\C#\TestProjects\Learn\ADO.NET\AlexandrGudok(example)\DB\Bank.mdf;Integrated Security=True;Connect Timeout=30";
+
+        public ArrayList GetAllDebitors()
+        {
+            ArrayList allDebitors = new ArrayList();
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand com = new SqlCommand("SELECT * FROM Debitors", con);
+
+            con.Open();
+
+            SqlDataReader dr = com.ExecuteReader();
+            if (dr.HasRows)
+            {
+                foreach (DbDataRecord result in dr)
+                {
+                    allDebitors.Add(result);
+                }
+            }
+            else
+            {
+                return null;
+            }
+            con.Close();
+
+            return allDebitors;
+        }
+    }
+}
---
+++ AlexandrGudok(example)/BankWinForms/MainForm.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Windows.Forms;
+
+namespace BankWinForms
+{
+    public partial class MainForm : Form
+    {
+        private DAL dal = new DAL();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            ArrayList allDebitors = dal.GetAllDebitors();
+
+            dgv_debitors.DataSource = allDebitors;
+        }
+    }
+}
